2512b7504642fb7d5cf117eb80ff1801177d99ae
freddie5k <fftiezzi@gmail.com>
2025-09-28 17:09:11 +0100

    venv VSCode

--- att.py
+++ att.py
@@ -3,7 +3,10 @@ import torch
 inputs = torch.tensor(
     [[0.43, 0.15, 0.89],
      [0.55, 0.87, 0.66],
-     [0.57, 0.85,0.64]]
+     [0.57, 0.85,0.64],
+     [0.22, 0.58, 0.33],
+     [0.77, 0.25, 0.10],
+     [0.05, 0.80, 0.55]]
 )
 
 query = inputs[1]
@@ -20,3 +23,7 @@ att_weoghts_2 = torch.softmax(attn_scores_2, dim=0)
 print(att_weoghts_2)
 print(att_weoghts_2.sum())
 
+context_vec_2 = torch.zeros(query.shape)
+for i, x_i in enumerate(inputs):
+    context_vec_2 += att_weoghts_2[i] * x_i
+print(context_vec_2)
